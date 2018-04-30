6b91e43112cc9648a3b32824c534b5d18c36db9d
lphexter <lphexter@yahoo.com>
2018-04-30 02:05:25 -0400

    final demo stuff

--- Matlab stuff/audioCluster.m
+++ Matlab stuff/audioCluster.m
@@ -19,6 +19,60 @@ fs = 8000; % default from recorder object is 8000
 % plot(speech);
 
 speech = speech(:,1);
+
+[pitch, MFCC] = computePitchMFCC(speech,fs);
+% Tw = 30; %frame duration in ms (from KINNUNEN clustering paper)
+% Ts = 10; %time shift
+% alpha = 0.97;
+% hamming = @(N)(0.54-0.46*cos(2*pi*[0:N-1].'/(N-1)));
+% 
+% R = [ 300 3700];    %range to consider
+% M = 20; % filterbank channels
+% C = 13; %cepstral coefficients
+% L = 22; % cepstral sine lifter parameter????
+% 
+% 
+% [MFCCs, FBEs, frames] = mfcc(speech, fs, Tw, Ts, alpha, hamming, R, M, C, L);
+s = size(MFCC);
+s = s(2);
+
+MFCCs =vertcat(pitch(1:s)', MFCC);
+
+MFCCs = (MFCCs-mean(MFCCs))./std(MFCCs);
+MFCCs = MFCCs';
+
+% running pca on the mfccs for plotting purposes
+pcaCoeffs = pca(MFCCs);
+pcaTest = MFCCs*pcaCoeffs;
+% disp(pcaTest);
+% plotting the pca
+s2 = size(pcaCoeffs);
+s2 = s2(2);
+if s2 >= 3
+    plot3(pcaTest(:,1), pcaTest(:, 2), pcaTest(:, 3), 'o', 'color', 'r');
+    
+end
+hold on;
+disp(size(MFCCs));
+end
+function [pitch1, mfcc1] = computePitchMFCC(x,fs)
+
+
+pwrThreshold = -50; % Frames with power below this threshold (in dB) are likely to be silence
+freqThreshold = 1000; % Frames with zero crossing rate above this threshold (in Hz) are likely to be silence or unvoiced speech
+
+% Audio data will be divided into frames of 30 ms with 75% overlap
+frameTime = 30e-3;
+samplesPerFrame = floor(frameTime*fs);
+startIdx = 1;
+stopIdx = samplesPerFrame;
+increment = floor(0.25*samplesPerFrame);
+overlapLength = samplesPerFrame - increment;
+
+[pitch1,~] = pitch(x,fs, ...
+    'WindowLength',samplesPerFrame, ...
+    'OverlapLength',overlapLength);
+
 Tw = 30; %frame duration in ms (from KINNUNEN clustering paper)
 Ts = 10; %time shift
 alpha = 0.97;
@@ -30,13 +84,31 @@ C = 13; %cepstral coefficients
 L = 22; % cepstral sine lifter parameter????
 
 
-[MFCCs, FBEs, frames] = mfcc(speech, fs, Tw, Ts, alpha, hamming, R, M, C, L);
+[mfcc1, FBEs, frames] = mfcc(x, fs, Tw, Ts, alpha, hamming, R, M, C, L);
 
-% running pca on the mfccs for plotting purposes
-pcaCoeffs = pca(MFCCs');
-pcaTest = MFCCs'*pcaCoeffs;
-% plotting the pca
-plot3(pcaTest(:,1), pcaTest(:, 2), pcaTest(:, 3), 'o', 'color', 'y');
-hold on;
+% mfcc1 = mfcc(x,fs,'WindowLength',samplesPerFrame, ...
+%     'OverlapLength',overlapLength, 'LogEnergy', 'Replace');
+% numFrames = length(pitch1);
+% voicing = zeros(numFrames,1);
+% 
+%     for i = 1: numFrames
+%         
+%         xFrame = x(startIdx:stopIdx,1); % 30ms frame
+% 
+%         if audiopluginexample.SpeechPitchDetector.isVoicedSpeech(xFrame,fs,... % Determining if the frame is voiced speech
+%                 pwrThreshold,freqThreshold)
+%             voicing(i) = 1;
+%         end
+%         startIdx = startIdx + increment;
+%         stopIdx = stopIdx + increment;
+% 
+%     
+%     end
+%     
+% pitch1(voicing == 0) = nan;
+% mfcc1(voicing == 0,:) = nan;
+
+
+end
 
 
--- Matlab stuff/testing.m
+++ Matlab stuff/testing.m
@@ -16,21 +16,67 @@
 % hold on;
 
 % plotting the MFCC vectors by speaker
+% figure();
+% c = pcaDataCell{2,1};
+% plot3(c(:,1),c(:,2),c(:,3),'o');
+% hold on;
+% for i = 2:size(pcaDataCell,2) %plot data points
+%     c = pcaDataCell{2,i};
+%     plot3(c(:,1),c(:,2),c(:,3),'o');
+% end
+% 
+%  for i = 1:size(pcaDataCell,2) %Plot averages for each speaker
+%     c = pcaDataCell{3,i};
+%     plot3(c(:,1),c(:,2),c(:,3),'o');
+% end
+% title('Scatter of MFCC Vectors Colored By Speaker')
+% hold on;
+
+% plotting with pca to visualize our feature vectors 
 figure();
-c = pcaDataCell{2,1};
-plot3(c(:,1),c(:,2),c(:,3),'o');
-hold on;
-for i = 2:size(pcaDataCell,2) %plot data points
-    c = pcaDataCell{2,i};
-    plot3(c(:,1),c(:,2),c(:,3),'o');
+names = unique(table2array(features(:,16)));
+len = size(names);
+
+CM = jet(len(1));
+indices = (1:len(1));
+map = containers.Map(names, indices);
+
+for i=1:size(features)
+    name = char(features{i, 16});
+    
+    color = CM(map(name),:);
+    temp = features(i,3:15);
+    temp = temp{:,:};
+    pcaCoeffs = pca(temp');
+    pcaTest = temp*pcaCoeffs;
+    % plotting the pca
+    plot3(pcaTest(:,1), pcaTest(:, 2), pcaTest(:, 3), 'o', 'color',color, 'MarkerFaceColor', color);
+    %[h, ~] = legend('show');
+    %all = [all pcaTest];
+    %legend('show');
+    
+    hold on;
 end
 
- for i = 1:size(pcaDataCell,2) %Plot averages for each speaker
-    c = pcaDataCell{3,i};
-    plot3(c(:,1),c(:,2),c(:,3),'o');
-end
-title('Scatter of MFCC Vectors Colored By Speaker')
-hold on;
+l_list = {};
+for i=1:size(names)
+%lgd = legend(names);
+    name= names{i};
+    disp(names{i});
+    l_list = [l_list, ['\color[rgb]{' num2str(CM(map(name), 1)) ',' num2str(CM(map(name), 2)) ',' num2str(CM(map(name), 3))  '} ' name]];
+    
+end 
+
+lgd = legend(l_list,'AutoUpdate','off');
+lgd.FontSize = 14;
+%colorbar
+% [h, ~, plots] = legend(names);
+% for idx = 1:length(h.String)
+%     h.String{idx} = [CM(map(h.String{idx}),:) ' ' h.String{idx}]
+% end
+
+%legend([hbc{:}],'old','new');
+%hold off;
 
 recObj = audiorecorder; % default is 9000 sample rate
 % if we want to set functions to tell us when we're starting and stopping
@@ -44,10 +90,12 @@ set(recObj,'TimerPeriod',1)%,'TimerFcn',{@audioCluster, recObj});
 % almost real-time watch your voice cluster 
 
 % will loop until you press 'q' 
+total = {};
 finish=false;
 set(gcf,'CurrentCharacter','@'); % set to a dummy character
 while ~finish
-  audioCluster(recObj);
+  t = audioCluster(recObj);
+  total = vertcat(total, t);
   % check for keys
   k=get(gcf,'CurrentCharacter');
   if k~='@' % has it changed from the dummy character?
@@ -56,7 +104,16 @@ while ~finish
     if k=='q', finish=true; end
   end
 end
+disp('about to predict');
+total = arrayfun(@(col) vertcat(total{:, col}), 1:size(total, 2), 'UniformOutput', false);
+total = total{1};
+predictedLabels = string(predict(trainedClassifier,total)); % Predict
+totalVals = size(predictedLabels,1);
 
+[predictedLabel, freq] = mode(predictedLabels); % Find most frequently predicted label
+match = freq/totalVals*100;
+
+disp(predictedLabel);
 
 %%
 
